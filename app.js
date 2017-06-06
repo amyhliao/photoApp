a11dd2151c3fc435b8352361a4292b728ce2884c
Amy Liao <amyhliao88@gmail.com>
2017-06-05 23:04:03 -0700

    Update format style in app js file

--- app.js
+++ app.js
@@ -10,41 +10,41 @@ app.get('/', function(req,res){
   //It will find and locate index.html from View or Scripts
 });
 
-app.get('/login', function(req, res){
+app.get('/login', function(req, res) {
   // res.sendFile('/login.html');
   res.send('This is the login page!');
 });
 
-app.get('/logout', function(req, res){
+app.get('/logout', function(req, res) {
   // res.sendFile('/logout.html');
   res.send('This is the logout page!');
 });
 
-app.get('/albums', function(req, res){
+app.get('/albums', function(req, res) {
   // res.sendFile('/albums.html');
   res.send('This is the albums page!');
 });
 
-app.get('/albums/new', function(req, res){
+app.get('/albums/new', function(req, res) {
   // res.sendFile('/new_albums.html');
   res.send('This is the create a new albums page!');
 });
 
-app.get('/albums/:id', function(req, res){
+app.get('/albums/:id', function(req, res) {
   // res.sendFile('/album_id.html');
   res.send('This is a specific albums page!');
 });
 
-app.get('/photos', function(req, res){
+app.get('/photos', function(req, res) {
   // res.sendFile('/photos.html');
   res.send('This is the photos page!');
 });
 
-app.get('/photos/new', function(req, res){
+app.get('/photos/new', function(req, res) {
   // res.sendFile('/new_photos.html');
   res.send('This is the create a new photos page!');
 });
-app.get('/photos/:id', function(req, res){
+app.get('/photos/:id', function(req, res) {
   // res.sendFile('/photo_id.html');
   res.send('This is a specific photos path!');
 });
